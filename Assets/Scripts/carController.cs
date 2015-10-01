189e29740285a4fb03ff1a06ca1959af545105a7
hhassan1230 <hhassan@oswego.edu>
2015-09-30 20:58:41 -0400

    puts in timer adds another level

--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -1,40 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class carController : MonoBehaviour {
 
 	public float tempVelocitiy;
 	public float torque;
+	public bool canDrive;
+	public Text countdownTimerText;
+	public int countdownDecNum;
 
 	private Rigidbody rb;
 	private Vector3 driveForce;
 
 	// Use this for initialization
 	void Start () {
+		countdownDecNum = 5;
+		canDrive = true;
 
 		rb = gameObject.GetComponent<Rigidbody>();
+		InvokeRepeating ("timerTick", 1, 1);
+		// when it start then when it repeats
+		countdownTimerText.text = countdownDecNum.ToString();
+	}
+
+	void timerTick(){
+		countdownDecNum--;
+		countdownTimerText.text = countdownDecNum.ToString();
+		if (countdownDecNum <= 0) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			driveForce = Vector3.forward * tempVelocitiy;
-			print("I am Driving");
-//			rb.AddForce(driveForce);
-			rb.AddRelativeForce(driveForce);
-		};
-		if (Input.GetKey(KeyCode.LeftArrow)) {
-			rb.AddTorque(transform.up * torque * -1);
-		};
-		if (Input.GetKey(KeyCode.RightArrow)) {
-			rb.AddTorque(transform.up * torque * +1);
-		};
+		if (canDrive) {
+			if (Input.GetKeyDown (KeyCode.LeftShift))
+				tempVelocitiy *= 2;
+			if (Input.GetKeyUp (KeyCode.LeftShift))
+				tempVelocitiy /= 2;
+			
+			if (Input.GetKey(KeyCode.UpArrow)) {
+				driveForce = Vector3.forward * tempVelocitiy;
+				rb.AddRelativeForce(driveForce);
+			};
+			if (Input.GetKey(KeyCode.DownArrow)) {
+				driveForce = Vector3.back * tempVelocitiy;
+				rb.AddRelativeForce(driveForce);
+			};
+			if (Input.GetKey(KeyCode.LeftArrow)) {
+				rb.AddTorque(transform.up * torque * -1);
+			};
+			if (Input.GetKey(KeyCode.RightArrow)) {
+				rb.AddTorque(transform.up * torque * +1);
+			};
+		}
 
 	}
-	void onCollisionEnter(Collision hit){
+
+	void OnCollisionEnter(Collision hit){
 		if (hit.gameObject.tag == "target") {
-			Application.LoadLevel(Application.loadedLevel);
+			print("I hit the target");
+			Application.LoadLevel(Application.loadedLevel +1);
+//			Application.LoadLevel("level2");
+		}
+	}
+	void OnTriggerEnter(Collider trigger){
+		if(trigger.gameObject.tag == "control"){
+			canDrive = false;
 		}
 	}
 }
